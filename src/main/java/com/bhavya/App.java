649a5163c5e8576c49f337d5002ad66ae61404b4
Bhavya Sachdeva <bsachdeva@expedia.com>
2018-05-25 11:17:54 +0530

    Try

---
+++ src/main/java/com/bhavya/App.java
@@ -0,0 +1,16 @@
+package com.bhavya;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Module.xml");
+    	 
+        HelloWorld obj = (HelloWorld) context.getBean("helloBean");
+        obj.printHello();
+        
+    }
+}
---
+++ src/main/java/com/bhavya/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.bhavya;
+
+public class HelloWorld 
+{
+	private String name;
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public void printHello() {
+		System.out.println("Hey ! " + name);
+	}
+}
